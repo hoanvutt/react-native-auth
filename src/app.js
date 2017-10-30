cb7210fbb89e6c4cef16c5c1e32fde78f03d8f55
Hoan Vu <hoanvutt@gmail.com>
2017-10-31 02:36:36 +0700

    Commit source

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  render () {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDoTlo5FyIsoldWnwymy36KmrcHDqs49P4',
+      authDomain: 'authentication-10e16.firebaseapp.com',
+      databaseURL: 'https://authentication-10e16.firebaseio.com',
+      projectId: 'authentication-10e16',
+      storageBucket: 'authentication-10e16.appspot.com',
+      messagingSenderId: '31100168712'
+    });
+  }
+
+  render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
